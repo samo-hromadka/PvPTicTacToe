f54697f29c61cb70f461501a1cd49434da14df6f
Herman999 <jirman.tomas@gmail.com>
2019-01-26 16:49:43 +0000

    tttp py

---
+++ ttt_game.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 26 16:36:41 2019
+
+@author: Tomas
+"""
+
+import numpy as np
+
+x = 4
+y = 4
+
+class ttt():
+    def __init__(x,y):
+        field = np.array([x,y])
+        
+
